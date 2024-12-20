640e2651301f437873b4d585e43c4dd70743e3f9
Janusz "Ivellios" Kamieński <janusz.kamienski@gmail.com>
2024-12-20 12:15:09 +0100

    dodanie pól: email, kod paczkomatu, dodatkowe informacje dla dostawcy; dodane więcej opisów i informacji opisowych w formularzu

--- .github/scripts/consts.py
+++ .github/scripts/consts.py
@@ -10,7 +10,9 @@ class OrgFormSchemaIds(enum.StrEnum):
     postal_code = "kod_pocztowy"
     city = "miasto"
     phone_number = "telefon"
-
+    email = "email"
+    package_box_code = "kod_paczkomatu"
+    additional_info = "dodatkowe_informacje"
 
 NEW_ORG_ISSUE_DEFAULT_TITLE = "[Nowa Organizacja]"
 NEW_ORG_FORM_SCHEMA_FILENAME = "nowa.yaml"
